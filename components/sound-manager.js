6c61e1b2d7577991f50f4d488a7eb7fb2e0a77ef
JohannesSchreurs <johannes@basedesign.com>
2019-05-27 17:54:26 +0200

    Added a sound manager component

---
+++ components/sound-manager.js
@@ -0,0 +1,9 @@
+const soundManager = new AFRAME.registerComponent('sound-manager', {
+
+
+    init: function () {
+        console.log(this.el);
+    }
+});
+
+export default soundManager;
--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 //Import our needed js files
 import Loader from '../components/Classes/Loader.js';
 import sceneManager from '../components/scene-manager.js';
+import particleEffect from '../components/particle-component.js';
+import soundManager from '../components/sound-manager.js';
 
-//Register the in our loader file
+//Register the modules in our loader class
 Loader.registerModules(
-    sceneManager
+    sceneManager,
+    particleEffect,
+    soundManager
 );
 
 //Append the JS to the DOM if it's done loading
