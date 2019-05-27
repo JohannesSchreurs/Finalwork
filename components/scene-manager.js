ccbf19cd10380486e1d742400b5635f5bd9f7d67
JohannesSchreurs <johannes@basedesign.com>
2019-05-27 14:54:10 +0200

    Added function to change a custom scene with code

--- components/scene-manager.js
+++ components/scene-manager.js
@@ -63,6 +63,7 @@ AFRAME.registerComponent('scene-manager', {
                         child.setAttribute('visible', 'false');
                     }
                 })
+
                 this.children[this.index].setAttribute('visible', 'true');
                 this.index++;
                 this.fade.components.material.material.opacity = 1;
@@ -74,6 +75,14 @@ AFRAME.registerComponent('scene-manager', {
         if (this.finished) {
             this.overlay.classList.remove('hidden');
             this.overlayText.innerHTML = `Done.`;
+        }
+    },
+
+
+    //Sometimes we will need do some custom stuff to the scene in the scene, so this function does just that
+    changeBasedOnScene: function (index, callback) {
+        if (this.index === index) {
+            callback();
         }
     }
 })
